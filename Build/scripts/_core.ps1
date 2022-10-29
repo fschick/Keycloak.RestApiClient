23b657162ae8d746fdfc6e46b8dd1c7bf8a53e6f
Florian Schick <florian.schick@schick-software.de>
2022-10-29 10:02:36 +0200

    Initial commit.

---
+++ Build/scripts/_core.ps1
@@ -0,0 +1,151 @@
+# Ensure unsigned powershell script execution ist allowed: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+#
+#region Build
+#
+
+function Npm-Restore([String] $folder) {
+	Push-Location $folder
+	
+	# Restore npm packages
+	& npm ci --prefer-offline --no-audit
+	if(!$?) {
+		Pop-Location
+		exit $LASTEXITCODE
+	}
+	
+	Pop-Location
+}
+
+function Build-Project([String] $project, [String] $version = "0.0.0") {
+    # Configure
+    $fileVersion = Get-FileVersion -version $version
+
+    # Create arguments
+    $arguments = @("-p:Version=$version", "-p:FileVersion=$fileVersion")
+
+    # Build with configuration "Debug"
+    Write-Host -ForegroundColor Green "dotnet build $project --configuration Debug $arguments"
+    & dotnet build $project --configuration Debug $arguments
+    if(!$?) {
+        exit $LASTEXITCODE
+    }
+
+    # Build with configuration "Release"
+    $arguments += "-warnaserror"
+    Write-Host -ForegroundColor Green "dotnet build $project --configuration Release $arguments"
+    & dotnet build $project --configuration Release $arguments
+    if(!$?) {
+        exit $LASTEXITCODE
+    }
+}
+
+#endregion
+
+#
+#region Tests
+#
+
+function Test-Project([String] $project, [String] $version, [String] $filter) {
+    # Configure
+    $fileVersion = Get-FileVersion -version $version
+
+    # Create arguments
+    $arguments = @("--logger", "trx", "-p:Version=$version", "-p:FileVersion=$fileVersion")
+
+    if ($filter) {
+        $arguments +=@("--filter", $filter)
+    }
+
+    # Run tests
+    Write-Host -ForegroundColor Green "dotnet test $project $arguments"
+    & dotnet test $project $arguments
+    if(!$?) {
+        exit $LASTEXITCODE
+    }
+}
+
+#endregion
+
+#
+#region Publish
+#
+
+function Publish-Project([String] $project, [String] $version, [String] $framework, [String] $runtime, [String] $publshFolder, [String] $publishProfile, [String] $password) {
+    # Configure
+    $fileVersion = Get-FileVersion -version $version
+    $configuration = "Release"
+
+    # Create arguments
+    $arguments = @("--configuration", $configuration, "--framework", $framework, "-p:Version=$version", "-p:FileVersion=$fileVersion")
+
+    $isSelfContained = !!$runtime
+    if ($isSelfContained) {
+        $arguments +=@("--self-contained", "--runtime", $runtime)
+    }
+
+    if ($publshFolder) {
+        $arguments +=@("--output", $publshFolder)
+    }
+	else {
+		$projectIsFile = Test-Path $project -PathType Leaf
+		$projectFolder = $(if($projectIsFile) {Split-Path -Path $project} else {$project})
+        $runtimeFolder = $(if($isSelfContained) {"/$runtime"} else {""})
+        $publshFolder =  "$projectFolder/bin/$configuration/$framework$runtimeFolder/publish"
+	}
+
+    if ($publishProfile) {
+        $arguments +=@("-p:PublishProfile=""$publishProfile""")
+    }
+
+    if ($password) {
+        $arguments +=@("-p:Password=""$password""")
+    }
+
+    # Publish project
+    Write-Host -ForegroundColor Green "dotnet publish $project $arguments"
+    & dotnet publish $project $arguments
+    if(!$?) {
+        exit $LASTEXITCODE
+    }
+
+    Remove-Item "$publshFolder/FS.TimeTracking.ReportServer.config.development.json" -ErrorAction Ignore
+}
+
+function Publish-Nuget([String] $project, [String] $version, [String] $publshFolder) {
+    # Configure
+    $fileVersion = Get-FileVersion -version $version
+    $configuration = "Release"
+
+    # Create arguments
+    $arguments = @("--configuration", $configuration, "-p:Version=$version", "-p:FileVersion=$fileVersion")
+
+    if ($publshFolder) {
+        $arguments +=@("--output", $publshFolder)
+    }
+
+    Write-Host -ForegroundColor Green "dotnet pack $project $arguments"
+    & dotnet pack $project $arguments
+}
+
+function Push-Nuget([String] $publshFolder, [String] $serverUrl, [String] $apiKey) {
+    # Create arguments
+    $arguments = @()
+
+    if ($serverUrl) {
+        $arguments += @("--source", $serverUrl);
+    }
+
+    if ($apiKey) {
+        $arguments += @("--api-key", $apiKey);
+    }
+
+    Write-Host -ForegroundColor Green "dotnet nuget push $publshFolder/*.nupkg $arguments"
+    & dotnet nuget push $publshFolder/*.nupkg $arguments
+}
+
+#endregion
+
+function Get-FileVersion([String] $version) {
+    return $version -replace '(\d+(?:\.\d+)*)(.*)', '$1'
+}
---
+++ Build/scripts/rest_api_client.publish.ps1
@@ -0,0 +1,51 @@
+# Ensure unsigned powershell script execution is allowed: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+param (
+	[Parameter(Mandatory=$false)] [String] $version,
+    [Parameter(Mandatory=$false)] [String] $publshFolder,
+	[Parameter(Mandatory=$false)] [String] $nugetUrl, 
+	[Parameter(Mandatory=$false)] [String] $apiKey,
+    [Parameter(Mandatory=$false)] [String] $openApiJson
+)
+
+. $PSScriptRoot/_core.ps1
+
+# Setup
+$workingDirectory = [System.IO.Path]::Combine("$PSScriptRoot", "..", "..")
+$localOpenApiJson = [System.IO.Path]::Combine($workingDirectory, "keycloak.openapi.json")
+$localOpenApiJsonCleaned = [System.IO.Path]::Combine($workingDirectory, "keycloak.openapi.clean.json")
+Push-Location $workingDirectory
+
+# Set input file
+if (!$openApiJson){
+    $openApiJson = "https://raw.githubusercontent.com/dahag-ag/keycloak-openapi/main/OpenApiDefinitions/keycloak-19.0.0.json"
+}
+
+# NPM install
+Npm-Restore Build/openapi-generator
+
+# Download or copy file
+if ($openApiJson.StartsWith("http")) {
+	Invoke-WebRequest -Uri $openApiJson -OutFile $localOpenApiJson
+} else {
+	Copy-Item $openApiJson $localOpenApiJson
+}
+
+# Clean up downloaded file (replace '<' and '>' in descriptions)
+$content = [System.IO.File]::ReadAllText($localOpenApiJson)
+$content = $content -replace '("?description"?\s*:\s*)(.*)<(.*)>(.*)', '$1$2{$3}$4'
+[System.IO.File]::WriteAllText($localOpenApiJsonCleaned, $content)
+
+# Generate client
+Push-Location Build/openapi-generator
+& node_modules/.bin/openapi-generator-cli generate -c openapi-generator.config.json -g csharp-netcore --type-mappings date-span='TimeSpan' -i $localOpenApiJsonCleaned -o ../../FS.Keycloak.RestApiClient
+Pop-Location
+
+# Fix smaller issues from open API generator
+& node Build/openapi-generator/openapi-generator-fix-shortcoming.js FS.Keycloak.RestApiClient
+
+Build-Project -project FS.Keycloak.RestApiClient -version $version
+Publish-Nuget -project FS.Keycloak.RestApiClient/src/FS.Keycloak.RestApiClient -version $version -publshFolder $publshFolder
+Push-Nuget -project -publshFolder $publshFolder -serverUrl $nugetUrl -apiKey $apiKey
+
+Pop-Location
