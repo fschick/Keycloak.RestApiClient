0ce114b26677be81e91759f59d387d83c957f217
Florian Schick <florian.schick@schick-software.de>
2023-11-02 21:09:38 +0100

    Documentation added to authentication parameters.

--- FS.Keycloak.RestApiClient/src/FS.Keycloak.RestApiClient/Model/AuthParameters.cs
+++ FS.Keycloak.RestApiClient/src/FS.Keycloak.RestApiClient/Model/AuthParameters.cs
@@ -12,22 +12,46 @@ namespace FS.Keycloak.RestApiClient.Model
         }
     }
 
+    /// <summary>
+    /// Authenticate against Keycloak using password grant flow.
+    /// </summary>
     public class PasswordGrant : AuthenticationFlow
     {
+        /// <summary>
+        /// The username of the user to authenticate.
+        /// </summary>
         public string UserName { get; set; }
 
+        /// <summary>
+        /// The password of the user to authenticate.
+        /// </summary>
         public string Password { get; set; }
     }
 
+    /// <summary>
+    /// Authenticate against Keycloak using client credentials flow.
+    /// </summary>
     public class ClientCredentials : AuthenticationFlow
     {
+        /// <summary>
+        /// The client id of the client to authenticate.
+        /// </summary>
         public string ClientId { get; set; }
 
+        /// <summary>
+        /// The client secret of the client to authenticate.
+        /// </summary>
         public string ClientSecret { get; set; }
     }
 
+    /// <summary>
+    /// Authenticate against Keycloak using a direct token.
+    /// </summary>
     public class DirectToken : AuthenticationFlow
     {
+        /// <summary>
+        /// The token to use for authentication.
+        /// </summary>
         public string Token { get; set; }
     }
 }
