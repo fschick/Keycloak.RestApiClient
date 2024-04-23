6fb56d485ae1a3ffc2690f1f04b3b5efa1b3eca4
Florian Schick <florian.schick@schick-software.de>
2024-04-23 10:07:04 +0200

    Further API fixes added.

--- Build/openapi-generator/openapi-generator-fix-spec.js
+++ Build/openapi-generator/openapi-generator-fix-spec.js
@@ -17,9 +17,16 @@ async function replace() {
 	delete openApiDefinition.components.schemas.AccessToken.properties.authTime
 	delete openApiDefinition.components.schemas.IDToken.properties.authTime
 	
+	// Fix realm import request body type
 	delete openApiDefinition['paths']['/admin/realms']['post']['requestBody']['content']['application/json']['schema']['format'];
 	delete openApiDefinition['paths']['/admin/realms']['post']['requestBody']['content']['application/json']['schema']['type'];
 	openApiDefinition['paths']['/admin/realms']['post']['requestBody']['content']['application/json']['schema']['$ref'] = '#/components/schemas/RealmRepresentation'
+	
+	// Remove deprecated members producing error on realm import, see https://github.com/fschick/Keycloak.RestApiClient/issues/4#issuecomment-2071597287
+	delete openApiDefinition['components']['schemas']['RealmRepresentation']['properties']['userCacheEnabled'];
+	delete openApiDefinition['components']['schemas']['RealmRepresentation']['properties']['realmCacheEnabled'];
+	delete openApiDefinition['components']['schemas']['RealmRepresentation']['properties']['oAuth2DeviceCodeLifespan'];
+	delete openApiDefinition['components']['schemas']['RealmRepresentation']['properties']['oAuth2DevicePollingInterval'];
 
     openApiDefinitionJson = JSON.stringify(openApiDefinition, null, '  ');
 	
